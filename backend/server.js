4340a829d42557393c7807c83791f816ba5a3928
Hamza <xsharif2551@gmail.com>
2025-10-28 16:38:51 +0300

    connecting express and react

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,18 @@
 import express from "express"
+import cors from "cors";
 const port  =  4500;
 
 
 const app = express()
 
 
+app.use(cors()); // allow frontend to talk to backend
+app.use(express.json());
 
-app.get("/",(req,res)=>{
-    console.log("Hi");
-    
-})
+app.get("/api/message", (req, res) => {
+  res.json({ message: "Hello from Express backend!" });
+});
+
 
 
 
---
+++ src/components/keyboard/keyboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function keyboard() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default keyboard
